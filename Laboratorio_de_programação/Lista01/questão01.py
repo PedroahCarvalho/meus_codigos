9455f3f68c9e3709b9a075250a8e95818e1837c9
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-04-05 20:25:43 -0300

    questão01

---
+++ Laboratorio_de_programação/Lista01/questão01.py
@@ -0,0 +1,10 @@
+#entrada
+print('para calculara a velocidade media entre com os valores.')
+km_rodados = float(input('Escreva quantos km foram percorridos: '))
+temp = float(input('Digite quanto tempo gasto no percursso: '))
+
+#processamento 
+vel_media = (km_rodados  / temp)
+
+#saida
+print(f'Sua velocida media nessa viagem sera de {vel_media} Km/h.')
