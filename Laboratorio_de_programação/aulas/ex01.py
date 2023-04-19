09e9f4b6bc74f424d56b55f743158da58a8beb6c
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-04-19 19:46:54 -0300

    EX

---
+++ Laboratorio_de_programação/aulas/ex01.py
@@ -0,0 +1,12 @@
+op = input('Deseja somar (S) ou multiplicar (m)?')
+if (op == 'S' or op == 'M'):
+    x = float(input('Digite o primeiro numero: '))
+    y = float(input('Digite o seguendo numero: '))
+    if (op == 'S'):
+        r = x + y
+    else:
+        r = x * y
+    print('O resultado é', r)
+else:
+    print('Opção invalida!')
+    
