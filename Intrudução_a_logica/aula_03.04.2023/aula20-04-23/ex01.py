ae2f2c4d1c0ba3a4135dff0132f9165d7fb6eb73
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-04-24 21:38:22 -0300

    ex

--- Intrudução_a_logica/aula_03.04.2023/aula20-04-23/ex01.py
+++ Intrudução_a_logica/aula_03.04.2023/aula20-04-23/ex01.py
@@ -3,19 +3,33 @@ linhas=0
 true=0
 false=0
 
+variaveis = input('digite se a sua variavel conten (2) ou (3) variaveis: ')
 formula = input('Digite a formula: ')
+if (variaveis == 2):
+    for a in vetorPossibilidade:
+        for b in vetorPossibilidade:
+            if eval(formula):
+                resultadoF=True
+                true+=1
+            else:
+                resultadoF=False
+                false+=1
+            print(f'A = {a} \t b = {b} \t H ={resultadoF}')
+            linhas+=1
 
-for a in vetorPossibilidade:
-    for b in vetorPossibilidade:
-        if eval(formula):
-            resultadoF=True
-            true+=1
-        else:
-            resultadoF=False
-            false+=1
-
-        print(f'A = {a} \t b = {b} \t H ={resultadoF}')
-        linhas+=1
+elif (variaveis == 3):
+    for a in vetorPossibilidade:
+        for b in vetorPossibilidade:
+            for c in vetorPossibilidade:
+                if eval(formula):
+                    resultadoF=True
+                    true+=1
+                else:
+                    resultadoF=False
+                    false+=1
+                print(f'A = {a} \t b = {b} \t H ={resultadoF}')
+                linhas+=1
+                
 if true == 0:
     print('esta formula e contraditoria')
 elif false == 0:
