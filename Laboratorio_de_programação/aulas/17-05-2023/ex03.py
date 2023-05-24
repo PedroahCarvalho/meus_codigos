e1853098134cddf9aa873f35613d148c68f20a53
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-05-24 19:08:34 -0300

    cs

--- Laboratorio_de_programação/aulas/17-05-2023/ex03.py
+++ Laboratorio_de_programação/aulas/17-05-2023/ex03.py
@@ -1,11 +1,21 @@
+"""
 faça um programa para calcular a serie de fibonacci para numero 
 informado pelo usuario, sendo F(0) = 0, F(n)= F(n-1) + F(n-2) Por exemplo, caso usuario informe o numero 
+"""
 
+fib = int(input('Digite o numeior de sequencia: '))
+a= 0
+b=1
 
-AntAnterior = 0
-Anterior = 0 
-Atual =0 
-
-
-Atual = Anterior + AntAnterior
-Anterior = Atual - Anterior
+if fib  >= 0:
+    print(a, end=', ')
+if fib >= 1:
+    print(b , end=', ')
+else:
+    print(a, end=', ') 
+    print(b, end=', ')
+    for cont in range(1, fib):
+        c = a + b 
+        a = b
+        b = c
+        print(c , end=', ')
