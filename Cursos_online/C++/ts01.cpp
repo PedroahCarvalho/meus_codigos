d1ede4bb6992bdfda229de1eec46f4de1752cf72
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-05-24 20:27:14 -0300

    alteração

--- Cursos_online/C++/ts01.cpp
+++
@@ -1,6 +0,0 @@
-# include <iosream>
-
-int main ()
-{
-    std::cout<<
-}
---
+++ Cursos_online/C/C++/ts01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+    printf("HELLO WORLD\n");
+
+
+}
+
+
+
+
+
+
