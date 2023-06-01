bc316ef7c1610c49db7597d45d0cfb98e9216f08
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-06-01 20:18:14 -0300

    strings

---
+++ Laboratorio_de_programação/aulas/31-05-2023/ExManipulaçãoStrings.py
@@ -0,0 +1,13 @@
+
+# upper
+# lower
+# find
+# strip , impotante função que retira espaços.
+# 
+
+# Ex. para manipulação de string, ex: variavel.upper  .  dodifica a retorno das strins para caixa alta mais sem rescrever na a varariavel
+nome= 'cebolinha'
+print(nome.upper()) 
+
+# Exemplo para modificação de string pra subistitur variavel.replace 
+nome = 'cebolinha Souza'
