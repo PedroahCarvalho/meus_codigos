c7bc723a512de4e464d013da32d654ae4a1914ec
PedroahCarvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-06-03 13:26:32 -0300

    lista

--- Laboratorio_de_programação/Listas_de_exercicios/lista02/ex02.py
+++ Laboratorio_de_programação/Listas_de_exercicios/lista02/ex02.py
@@ -1,2 +1,14 @@
 #2- Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
 
+
+
+while True:
+    nome=input('Digite seu nome de ususario: ')
+    senha=input('Crie uma senha: ')
+    if nome == senha :
+        print('ERRO: a senha deve ser diferente de nome de usuario digite novamente: ')
+    else:
+        print('Dados dados gravados com sucesso!')
+        break
+
+print('Fim da execução do bloco.')
