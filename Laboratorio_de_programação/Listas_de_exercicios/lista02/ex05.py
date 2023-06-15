783cd5ea0ea16939ea37626820eec32222e8cd00
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-06-14 23:46:52 -0300

    f

--- Laboratorio_de_programação/Listas_de_exercicios/lista02/ex05.py
+++ Laboratorio_de_programação/Listas_de_exercicios/lista02/ex05.py
@@ -1,2 +1,13 @@
 #5- Faça um programa que calcule e mostre a média aritmética de N notas. N equivale ao total de avaliações. 
  
+
+QntNotas = int(input('Insira  a quantidade notas a ser inseridas: '))
+notas = 0
+if QntNotas >= 0:
+    for a in range(0,QntNotas):
+        if QntNotas <= 0:
+            notas = float(input('insira a sua nota: '))
+        else:
+            Notas = float(input('insira a proxima nota: '))
+else:
+    print('valor invalido!')
