45644a8475e060d8f4bbe4edf007abf3107a8189
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-08-10 20:44:22 -0300

    t

--- EstruturaDeDados-I/aula04/ex01.py
+++ EstruturaDeDados-I/aula04/ex01.py
@@ -5,4 +5,23 @@ acertos = [] # lista vazia.
 for _ in range(len(segredo)):
     acertos.append('_')
 
+while '-' in acertos:
+    chute = input('qual a letra? ')
+    posicao = 0
+    for letra in segredo:
+        print(letra)
+        if chute.upper() == letra.upper():
+            acertos[posicao] = chute
+        posicao = posicao + 1
+        if (chute not in acertos) and (chute not in erros):
+            print((f' acertods {acertos}'))
+            print(f'erros {erros}')
+            if len(erros) > 10:
+                print("enforcou! game over")
+                break
+    
+    
+
+
+
 print(acertos)
