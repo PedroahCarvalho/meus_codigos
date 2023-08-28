e4ee7642be442ce5c6d7f3587471b83a7c19e031
PedroahCarvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-08-28 19:06:51 -0300

    k
    k

---
+++ EstruturaDeDados-I/aula04/exercicio01.py
@@ -0,0 +1,21 @@
+"""
+Crie duas lista em python, uma para armazenar o nome  outra lista para armazenar a idade de 5 pessoas. Posteriormente indentifique quais pessoas tem 18 anos ou mais e quantas pessos são menores de indade.
+Ex:
+jose, 10 anos 
+Joaquim 19 anos
+Jailton,  40 anos 
+juaresz, 5 anos 
+João, 18 anos 
+--> São maiores de 18 anos joaquim, jailton, João
+--> São menores de odade, juarez
+"""
+listaNomes = []
+listaIdade = []
+N_pessoas = 5
+
+for i in range(len(N_pessoas)):
+    listaNomes.append(input)
+
+
+
+#tentar fazer com lista dentro de lista
