582c72b1cf0b50cfae4f448bf915ed959b8b91c2
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-09-26 14:17:13 -0300

    t

--- Introdução_matematica/trabalho2/juros.py
+++ Introdução_matematica/trabalho2/juros.py
@@ -1,5 +1,4 @@
 
-
 opcao = 1
 while opcao != 6:
     print('========== PROGRA DE CALCULO DE JUROS ==========')
@@ -8,22 +7,9 @@ while opcao != 6:
 
     if opcao == 1:
         print("=== JUROS SIMPLES ===")
-        capital = input("Digite o capital:")
-        taxjuros = input("Digite a taxa de juros")
-        tempo = input("Digite por quanto tempo:")
-        #j = c * i * t
-        juros = capital * taxjuros * tempo
-        print(f'Seu juros sera de {juros}')
-
-    elif opcao == 2:
-        print('')
-        capital = input("Digite o capital: ")
-        juros  = input("Digite o juros: ")
-        montante = capital + juros
-        
-
-def jurosSimple(capital, taxaJuros, tempo)
-        #j = c * i * t
-        juros = capital * taxaJuros * tempo
-        return juros
-    
+        capital = float(input("Digite o capital:"))
+        taxjuros = float((input("Digite a taxa de juros:")))
+        tempo = float(input("Digite por quanto tempo:"))
+        taxa = taxjuros/100
+        juros = capital * taxa * tempo
+        print(juros)
