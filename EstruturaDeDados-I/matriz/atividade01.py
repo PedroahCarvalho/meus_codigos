83801d7728bf1433da658cf6e42c814b44ae2ce9
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2023-11-22 19:12:06 -0300

    t

---
+++ EstruturaDeDados-I/matriz/atividade01.py
@@ -0,0 +1,12 @@
+"""
+criar uma matriz N X M preenchidas com zeros
+N --> numero de linhas  
+M --> numero de colunas
+E --> imprimir na forma de matriz
+"""
+n = 5
+m = 4
+matriz = []
+for linha in range(n)
+
+# adicione elementos de forma aleatoria em uma matriz NxM e retor
---
+++ EstruturaDeDados-I/matriz/matriz01.py
@@ -0,0 +1,9 @@
+#numpy pandas bibliotecas boas para se estudar sobre matrizes
+
+lista = [["nathan", "luciano", "Regina","nauhan"],["pedro","Gleiber", "Francine","Sarah"]] 
+print(lista[1][0])
+
+print(lista[0][0])
+lista[0]= lista[0] + "isabelle"
+
+print(lista[0])
